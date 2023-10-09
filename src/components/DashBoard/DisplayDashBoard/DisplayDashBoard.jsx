c7a65fc006d8972bf9d86d645ca20b68ddd85fce
Aaryan87 <102862016+Aaryan87@users.noreply.github.com>
2023-10-09 15:19:23 +0530

    New Change

--- src/components/DashBoard/DisplayDashBoard/DisplayDashBoard.jsx
+++ src/components/DashBoard/DisplayDashBoard/DisplayDashBoard.jsx
@@ -6,14 +6,14 @@ import { useNavigate } from 'react-router-dom';
 function DisplayDashBoard(props){
     const history = useNavigate();
     const temp = ()=>{
-        history("/studentlist")
+        history("/studentlist");
       }
     return(
         <div>
             <NavBar/>
-            <Card title="Add Student" function={temp} content="Check Here"  />
-            <Card title="Add Staff" content="Check Here" />
-            <Card title="Assignment" content="Check Here" />
+            <Card title="Add Student" function={temp} content="Check Here"/>
+            <Card title="Add Staff" content="Check Here"/>
+            <Card title="Assignment" content="Check Here"/>
         </div>
     );
 }
